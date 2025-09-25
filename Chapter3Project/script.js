33f0c3b4624fbf476f2954c8e54ce7bf6447b1b7
yazmina333 <yazmin.m.alfaro@gmail.com>
2025-09-24 20:50:10 -0500

    Add files via upload

---
+++ Chapter3Project/script.js
@@ -0,0 +1,29 @@
+     //Yazmin Alfaro 09/09/25//
+     
+     // 1. A variable to store your full name
+      var fullName = "John Doe";
+      console.log("Full Name:", fullName);
+
+      // 2. A variable to store your desired annual salary
+      var desiredSalary = 80000;
+      console.log("Desired Salary:", desiredSalary);
+
+      // 3. A variable to store your veteran status (its a boolean so its either true or false)
+      var isVeteran = false;
+      console.log("Veteran Status:", isVeteran);
+      
+      // 4. An array to store the names of three of your friends
+      var friendsNames = ["Nevaeh", "Amberleigh", "Kai"];
+      console.log("Friends' Names:", friendsNames);
+      
+      // 5. An array to store the value of the desired annual salary for your three friends
+      var friendsSalaries = [60000, 70000, 80000];
+      console.log("Friends' Salaries:", friendsSalaries);
+      
+      // 6. A literal object to store the first name, last name, and desired annual salary of yet another friend
+      var fourthFriend = {
+        firstName: "Leonie",
+        lastName: "Divine",
+        desiredAnnualSalary: 90000
+      };
+      console.log("Fourth Friend:", fourthFriend);
